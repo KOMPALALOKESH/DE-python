a01c63dae90b644380593f4f624158b05d5fbc45
KOMPALALOKESH <kompalalokesh123@gmail.com>
2024-01-23 12:22:13 +0530

    second commit

--- classes.py
+++ classes.py
@@ -1,1 +1,17 @@
+'''
+# Fetch changes from the remote without merging them
+git fetch origin main
+
+# Merge the changes into your local branch
+git merge origin/main
+
+# Now you can push your changes
+git push origin master:main
+'''
+# alternative command
+'''
+git pull origin main --allow-unrelated-histories
+git push origin master:main
+'''
+
 print("Hello world!")
